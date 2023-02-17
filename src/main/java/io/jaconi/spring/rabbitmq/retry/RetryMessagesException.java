eac068f018bbb9c9aa69c959fb86542f7f17277e
Julian Nodorp <jnodorp@jaconi.io>
2023-02-17 13:23:13 +0100

    feat: add support for plain AMQP messages

--- src/main/java/io/jaconi/spring/rabbitmq/retry/RetryMessagesException.java
+++ src/main/java/io/jaconi/spring/rabbitmq/retry/RetryMessagesException.java
@@ -1,27 +1,50 @@
 package io.jaconi.spring.rabbitmq.retry;
 
+import org.springframework.amqp.support.converter.MessagingMessageConverter;
+import org.springframework.messaging.Message;
+
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
-import org.springframework.messaging.Message;
 
 public class RetryMessagesException extends RuntimeException {
+    private static final MessagingMessageConverter CONVERTER = new MessagingMessageConverter();
     private final Collection<Message<?>> messages;
 
     public <T> RetryMessagesException(Message<T> message) {
         this(Collections.singleton(message));
     }
 
+    @SuppressWarnings("unchecked")
+    public <T> RetryMessagesException(org.springframework.amqp.core.Message message) {
+        this((Message<T>) CONVERTER.fromMessage(message));
+    }
+
     public <T> RetryMessagesException(String message, Message<T> message1) {
         this(message, Collections.singleton(message1));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> RetryMessagesException(String message, org.springframework.amqp.core.Message message1) {
+        this(message, (Message<T>) CONVERTER.fromMessage(message1));
     }
 
     public <T> RetryMessagesException(Throwable cause, Message<T> message) {
         this(cause, Collections.singleton(message));
     }
 
+    @SuppressWarnings("unchecked")
+    public <T> RetryMessagesException(Throwable cause, org.springframework.amqp.core.Message message) {
+        this(cause, (Message<T>) CONVERTER.fromMessage(message));
+    }
+
     public <T> RetryMessagesException(String message, Throwable cause, Message<T> message1) {
         this(message, cause, Collections.singleton(message1));
+    }
+
+    @SuppressWarnings("unchecked")
+    public <T> RetryMessagesException(String message, Throwable cause, org.springframework.amqp.core.Message message1) {
+        this(message, cause, (Message<T>) CONVERTER.fromMessage(message1));
     }
 
     public <T> RetryMessagesException(Collection<Message<T>> messages) {
